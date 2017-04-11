928469280ae058e85922e72477b369451ab6c674
yoann1995 <yoann.boyere95@gmail.com>
2017-04-11 16:05:45 +0200

    Rectif merge V2

--- nuage/src/app/app.module.ts
+++ nuage/src/app/app.module.ts
@@ -3,9 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { APIService } from 'app/project/model/api.service';
-
-import { AppComponent } from './project/connexionPageComponent/app.component';
-import { ConnexionComponentDriveComponent } from './project/connexion-component-drive/connexion-component-drive.component';
 import { FilePageComponent } from './project/file-page/file-page.component';
 import { AppComponent } from 'app/project/connexionPageComponent/app.component';
 import { ConnexionComponentDriveComponent } from 'app/project/connexion-component-drive/connexion-component-drive.component';
