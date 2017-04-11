a9b2662b5ce9e1205023ee6648832bb4c91217b8
yoann1995 <yoann.boyere95@gmail.com>
2017-04-11 16:04:05 +0200

    Rectif probleme merge

--- nuage/src/app/app.module.ts
+++ nuage/src/app/app.module.ts
@@ -4,14 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { APIService } from 'app/project/model/api.service';
 
-<<<<<<< HEAD:nuage/src/app/app.module.ts
 import { AppComponent } from './project/connexionPageComponent/app.component';
 import { ConnexionComponentDriveComponent } from './project/connexion-component-drive/connexion-component-drive.component';
 import { FilePageComponent } from './project/file-page/file-page.component';
-=======
 import { AppComponent } from 'app/project/connexionPageComponent/app.component';
 import { ConnexionComponentDriveComponent } from 'app/project/connexion-component-drive/connexion-component-drive.component';
->>>>>>> bf525fe075f0b00c2ff1eb98df536da22fa1bac7:nuage/src/app/app.module.ts
 
 @NgModule({
   declarations: [
