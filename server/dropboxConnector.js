a6503e9a3a7467d5f9ddc2c91e97c4fd6b0a87a9
Alexis BRAULT <alexis.brault@wanadoo.fr>
2017-04-14 17:04:55 +0200

    DROPBOX + WIKI

---
+++ server/dropboxConnector.js
@@ -0,0 +1,110 @@
+var https = require('https');
+var querystring = require('querystring');
+
+var client_id = '3utkchwe9s4upxs';
+var redirect_uri = 'http://localhost:8080/authDropbox';
+var client_secret = '9pdyuatgrykfflb';
+
+var rest_api_url = 'https://content.dropboxapi.com/1';
+
+class DropboxConnector {
+  constructor() {
+    this.bearer = ''; 
+  }
+
+  static getConnexionURL(){
+  return 'https://www.dropbox.com/1/oauth2/authorize?response_type=code&redirect_uri=' + redirect_uri + '&client_id=' + client_id;
+ }
+
+ getToken(code, res) {
+  var data = querystring.stringify({
+    client_secret: client_secret,
+    grant_type: 'authorization_code',
+    client_id: client_id,
+    code: code,
+    redirect_uri: redirect_uri
+  });
+  var options = {
+    host: 'api.dropboxapi.com',
+    path: '/1/oauth2/token',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(data)
+    },
+    method: 'POST',
+    port: 443
+  };
+  console.log('data',data);
+  this.httpRequest(data, options, this.setBearer.bind(this), res);
+ }
+
+ setBearer(b, res){
+  let json = JSON.parse(b);
+  this.bearer = json.access_token;
+  console.log('bearer',this.bearer);
+  res.end('Bearer OK')
+ }
+
+/*** FUN LIST ***/
+
+about(res){
+  this.rest_api('GET', 'about', this.writeJSON, res);
+}
+
+files(res){
+  this.rest_api('GET', 'files/auto/', this.writeJSON, res);
+}
+
+writeJSON(json, res){
+ res.end(json);
+}
+
+rest_api(method, f, callback, res, data){
+  var options = {
+    host: 'content.dropboxapi.com',
+    path: '/1/'+f+'?access_token=' + this.bearer,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    method: method,
+    port: 443
+  };
+  this.httpRequest(data, options, callback, res);
+}
+
+/****** UTIL ******/
+
+httpRequest(data, options, callback, response) {
+  var req = https.request(options, function(res) {
+    res.setEncoding('utf8');
+    var content = '';
+    res.on('data', function(chunk) {
+      content += chunk;
+    });
+    res.on('end', function() {
+      if (res.statusCode === 200) {
+        if (typeof callback === 'undefined')
+          console.log(content);
+        else
+          callback(content, response);
+      } else {
+        console.log('Status:', res.statusCode);
+        console.log(content);
+      }
+    });
+  }).on('error', function(err) {
+    console.log('Error:', err);
+  });;
+
+  if (typeof data === 'undefined'){
+  
+  }else{
+    req.write(data);
+  }
+  req.end()
+ }
+}
+
+//GoogleDriveConnector.prototype.setBearer.bind(this);
+
+module.exports = DropboxConnector;
--- server/quickstart2.js
+++ server/quickstart2.js
@@ -1,13 +1,17 @@
 var fs = require('fs');
 var express = require('express');
-var GoogleDriveConnector = require("./googleDriveConnector");
 var rand = require("generate-key");
 var session = require('client-sessions');
 var https = require('https');
 
+/**** CONNECTOR ****/
+var GoogleDriveConnector = require("./googleDriveConnector");
+var DropboxConnector = require("./dropboxConnector");
+
 var app = express();
 
 GDC = new GoogleDriveConnector();
+DC = new DropboxConnector();
 
 app.use(session({
   cookieName: 'session',
@@ -22,17 +26,42 @@ function msgJSON(key, msg) {
   return o;
 }
 
-app.get('/authGoogleDrive', function(req, res) {
-  GDC.getToken(req.query.code, res);
-  //res.end();
+app.get('/', function(req, res){
+  fs.readFile('wiki.html', function(err, content) {
+    if (err) {
+      console.log('Error loading client secret file: ' + err);
+      return;
+    }
+    res.end(content);
+  });
 });
 
-app.get('/listFiles', function(req, res) {
+app.get('/authGoogleDrive', function(req, res) {
+  GDC.getToken(req.query.code, res);
+});
+
+app.get('/authDropbox', function(req, res) {
+  DC.getToken(req.query.code, res);
+});
+
+app.get('/listFiles/GoogleDrive', function(req, res) {
   GDC.files(res);
 });
 
-app.get('/connect', function(req, res) {
+app.get('/listFiles/Dropbox', function(req, res) {
+  DC.files(res);
+});
+
+app.get('/about', function(req, res) {
+  GDC.about(res);
+});
+
+app.get('/connect/GoogleDrive', function(req, res) {
   res.end('<a href="'+GoogleDriveConnector.getConnexionURL()+'">Link</a>')
+});
+
+app.get('/connect/Dropbox', function(req, res) {
+  res.end('<a href="'+DropboxConnector.getConnexionURL()+'">Link</a>')
 });
 
 
