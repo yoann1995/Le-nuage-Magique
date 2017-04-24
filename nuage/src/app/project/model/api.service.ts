8e6c65af94ac9f9adc32f90a270271087a0ad774
Florent CATIAU-TRISTANT <florent.ct@orange.fr>
2017-04-24 09:53:56 +0200

    Starting folder creation + remove file on Dropbox

--- nuage/src/app/project/model/api.service.ts
+++ nuage/src/app/project/model/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { FileDrive } from '../model/FileDrive';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
@@ -21,6 +22,25 @@ export class APIService {
 	public getAccountInfos(){
 		console.log("Reaching "+this.nuageUrl+"accountInfos/");
 		return this.http.get(this.nuageUrl+"accountInfos/")
+                  .map(this.extractJson)
+                  .catch(this.handleError);
+	}
+
+	public removeFile(file:FileDrive){
+		let url:string;
+		for(let i=0;i<file.sources.length;i++){
+			let src = file.sources[i];
+			if(src.name === "GoogleDrive"){
+				url = "delete/GoogleDrive?id="+src.id;
+			} else if (src.name === "Dropbox" ){
+				url = "delete/Dropbox?path="+src.id;
+			}
+		}
+
+		//url = url.replace(/ /g,"+");
+
+		console.log("Reaching "+this.nuageUrl+url);
+		return this.http.get(this.nuageUrl+url)
                   .map(this.extractJson)
                   .catch(this.handleError);
 	}
