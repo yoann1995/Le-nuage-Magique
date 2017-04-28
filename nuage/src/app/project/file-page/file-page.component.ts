3d18ba6700221438e985ddaf74f1c97abe4b0688
yoann1995 <yoann.boyere95@gmail.com>
2017-04-28 10:52:13 +0200

    Upload file

--- nuage/src/app/project/file-page/file-page.component.ts
+++ nuage/src/app/project/file-page/file-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FileDrive } from '../model/FileDrive';
-import { Http, Response } from '@angular/http';
+import { Http, Response, RequestOptions} from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { APIService } from '../model/api.service'
 
@@ -19,7 +19,7 @@ export class FilePageComponent implements OnInit {
   private dropboxFilter:boolean = true;
   private onedriveFilter:boolean = true;
 
-  constructor(public api: APIService)  {
+  constructor(public api: APIService, private http: Http)  {
     this.rootFolder = new FileDrive("Root",new Array<FileDrive>(),"folder",0, null); //We always start the app from the root
     this.stackFolder = new Array<FileDrive>();
   }
@@ -196,5 +196,25 @@ export class FilePageComponent implements OnInit {
       }
     }
     return res;
+  }
+
+  fileChange(event) {
+    let fileList: FileList = event.target.files;
+    if(fileList.length > 0) {
+        let file: File = fileList[0];
+        let formData:FormData = new FormData();
+        formData.append('uploadFile', file, file.name);
+        let headers = new Headers();
+        headers.append('Content-Type', 'multipart/form-data');
+        headers.append('Accept', 'application/json');
+        let options = new RequestOptions(headers);
+        this.http.post(`http://localhost:8080/upload/GoogleDrive`, formData, options)
+            .map(res => res.json())
+            .catch(error => Observable.throw(error))
+            .subscribe(
+                data => console.log('success'),
+                error => console.log(error)
+            )
+    }
   }
 }
